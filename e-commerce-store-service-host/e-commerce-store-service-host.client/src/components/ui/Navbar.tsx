b6ab3f9ae5fc62e24da8a3b981c6d816d3a75ca5
Jaden Davis <jdavis126@unl.edu>
2025-04-18 11:46:58 -0500

    added dark/light mode toggle in Navbar.tsx

--- e-commerce-store-service-host/e-commerce-store-service-host.client/src/components/ui/Navbar.tsx
+++ e-commerce-store-service-host/e-commerce-store-service-host.client/src/components/ui/Navbar.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom"
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from "../mode-toggle"
 
 export default function Navbar() {
   return (
     <header className="flex items-center justify-between px-6 py-4 shadow-md bg-white">
-      <Link to="/" className="text-xl font-bold text-primary">eCommerceStore</Link>
+      
+      <div className="flex items-center gap-4">
+              <Link to="/" className="text-xl font-bold">eCommerceStore</Link>
+              <ModeToggle></ModeToggle>
+              </div>
       <div className="flex items-center gap-4">
         <Link to="/login"><Button variant="outline">Sign In</Button></Link>
         <Link to="/register"><Button variant="outline">Sign Up</Button></Link>
