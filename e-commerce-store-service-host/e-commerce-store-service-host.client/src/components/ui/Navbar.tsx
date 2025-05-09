c24d289444d918af02188b2ad159f1f0a08ab55a
Datquangbui1011 <dbui5@huskers.unl.edu>
2025-05-09 13:22:44 -0500

    Narbar update

--- e-commerce-store-service-host/e-commerce-store-service-host.client/src/components/ui/Navbar.tsx
+++ e-commerce-store-service-host/e-commerce-store-service-host.client/src/components/ui/Navbar.tsx
@@ -1,20 +1,34 @@
-import { Link } from "react-router-dom"
-import { Button } from "@/components/ui/button"
-import { ModeToggle } from "../mode-toggle"
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
+import { useAuth } from "@/context/AuthenContext";
 
 export default function Navbar() {
+  const { isLoggedIn } = useAuth();
+
   return (
     <header className="flex items-center justify-between px-6 py-4 shadow-md bg-white">
-      
       <div className="flex items-center gap-4">
-              <Link to="/" className="text-xl font-bold">eCommerceStore</Link>
-              <ModeToggle></ModeToggle>
-              </div>
+        <Link to="/" className="text-xl font-bold">
+          eCommerceStore
+        </Link>
+        <ModeToggle />
+      </div>
       <div className="flex items-center gap-4">
-        <Link to="/login"><Button variant="outline">Sign In</Button></Link>
-        <Link to="/register"><Button variant="outline">Sign Up</Button></Link>
-        <Link to="/cart"><Button>Cart</Button></Link>
+        {!isLoggedIn ? (
+          <>
+            <Link to="/login">
+              <Button variant="outline">Sign In</Button>
+            </Link>
+            <Link to="/register">
+              <Button variant="outline">Sign Up</Button>
+            </Link>
+          </>
+        ) : null}
+        <Link to="/cart">
+          <Button>Cart</Button>
+        </Link>
       </div>
     </header>
-  )
+  );
 }
